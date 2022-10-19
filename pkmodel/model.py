05a363343f9282e66c431b3e5ec1f400d22f3335
mwhitemfldm <miawhitefield@gmail.com>
2022-10-19 14:23:53 +0100

    add tests to model_unit test

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -20,7 +20,7 @@ class Model:
 
     """
 
-    def __init__(self, central, peripherals, dosing=[]):
+    def __init__(self, central, peripherals, dosing):
         self.central = central
         self.peripherals = peripherals
         self.dose = dosing
@@ -28,3 +28,29 @@ class Model:
     @property
     def pcount(self):
         return len(self.peripherals)
+
+
+""" Redundant subclasses """ 
+# class Central_Compartment:
+#     """A Pharmokinetic (PK) model
+
+#     Parameters
+#     ----------
+
+#     value: numeric, optional
+#         an example paramter
+
+#     """
+#     def __init__(self, V: float, CL: float):
+#         self.volume = V
+#         self.clearance = CL
+
+# class Peripheral_Compartment:
+#     """ Compartments """
+#     def __init__(self, V: float, Q: float):
+#         self.volume = V
+#         self.transrate = Q
+
+# class Dosing_Compartment:
+#     def __init__(self, ka=None):
+#         self.abs = ka
