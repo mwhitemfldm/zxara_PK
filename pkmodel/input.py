d830c74bc6bbaf6ec4cb42f5e920b750c9a51cc4
Jacob Wright <vlad@tran.sylvan.ia>
2022-10-19 14:39:38 +0100

    Update input.py

--- pkmodel/input.py
+++ pkmodel/input.py
@@ -1,42 +1,46 @@
 
-''' 
-Script for user to input all parameter values and store them
-'''
+def model_input():
+    """
+    Returns parameter arrays from unit inputs for central, peripheral and dosage compartments
+    """
+    # Creating central array
+    VC = float(input('Volume of central compartment in mL: '))
+    CL = float(input('Clearance rate from central compartment in mL/h: '))
+    central = [VC, CL]
 
-# META QUESTIONS ON MODEL
+    # Creating peripherals array
+    NUM_OF_PCS = int(input('Number of peripheral compartments (0, 1, or 2): '))
 
-NUM_OF_PCS = int(input('Number of peripheral compartments (0, 1, or 2): '))
+    peripherals = []
+    for i in range(0,NUM_OF_PCS):
+        VP = float(input('Volume of peripheral compartment {} in mL: '.format(i+1)))
+        QP = float(input('Transition rate between central compartment and peripheral compartment {} in mL/h: '.format(i+1)))
+        peripherals.append([VP,QP])
 
-DOSAGE_COMPARTMENT = input('Is there a dosage compartment (Yes/No)? ')
+    # Creating dosing array
+    DOSAGE_COMPARTMENT = input('Is there a dosage compartment (Yes/No)? ')
 
-# TODO: what if it is a combination of both types of dosage
-STEADY_DOSAGE = input('Is drug given in steady application over time (Yes/No)? ')
+    dosing = []
+    if DOSAGE_COMPARTMENT == 'Yes':
+        KA = float(input('Absorption rate for dosage compartment (/h) if subcutaneous dosing: '))
+        dosing.append(KA)
 
-# SPECIFIC INPUTS BASED ON MODEL TYPE
+    return central, peripherals, dosing
 
-# Creating central array
-VC = float(input('Volume of central compartment in mL: '))
-CL = float(input('Clearance rate from central compartment in mL/h: '))
-central = [VC, CL]
+def dosage_input():
+    """ 
+    Generates dosage function from user input 
+    """
 
-# Creating peripherals array
-peripherals = []
-for i in range(0,NUM_OF_PCS):
-    VP = float(input('Volume of peripheral compartment {} in mL: '.format(i+1)))
-    QP = float(input('Transition rate between central compartment and peripheral compartment {} in mL/h: '.format(i+1)))
-    peripherals.append([VP,QP])
+    STEADY_DOSAGE = input('Is drug given in steady application over time (Yes/No)? ')
 
-# Creating dosing array
-dosing = []
-if DOSAGE_COMPARTMENT == 'Yes':
-    KA = float(input('Absorption rate for dosage compartment (/h) if subcutaneous dosing: '))
-    dosing.append(KA)
+    if STEADY_DOSAGE == 'Yes':
+        DOSE = float(input('Dose of drug in ng given at each hour: '))
+        TOTAL_TIME = float(input('Time period during which drug is given (h): '))
+    else: 
+        DOSE = int(input('Instantaneous dose of drug given per time point (ng): '))
+        TIME_POINTS = list(input('List of time points at which drug is given (h): '))
 
+    # TODO: what if it is a combination of both types of dosage
 
-if STEADY_DOSAGE == 'Yes':
-    DOSE = float(input('Dose of drug in ng given at each hour: '))
-    TOTAL_TIME = float(input('Time period during which drug is given (h): '))
-else: 
-    DOSE = int(input('Instantaneous dose of drug given per time point (ng): '))
-    TIME_POINTS = list(input('List of time points at which drug is given (h): '))
 
