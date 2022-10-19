5841d94a9529342f060fbfc13bb4939adb2ecb43
Jacob Wright <vlad@tran.sylvan.ia>
2022-10-19 14:29:51 +0100

    Modify model.py

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -27,29 +27,3 @@ class Model:
     @property
     def pcount(self):
         return len(self.peripherals)
-
-
-""" Redundant subclasses """ 
-# class Central_Compartment:
-#     """A Pharmokinetic (PK) model
-
-#     Parameters
-#     ----------
-
-#     value: numeric, optional
-#         an example paramter
-
-#     """
-#     def __init__(self, V: float, CL: float):
-#         self.volume = V
-#         self.clearance = CL
-
-# class Peripheral_Compartment:
-#     """ Compartments """
-#     def __init__(self, V: float, Q: float):
-#         self.volume = V
-#         self.transrate = Q
-
-# class Dosing_Compartment:
-#     def __init__(self, ka=None):
-#         self.abs = ka
