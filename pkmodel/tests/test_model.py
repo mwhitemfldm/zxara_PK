11c79947c9bc085db6b92d3849bcb59954fb6b4a
mwhitemfldm <miawhitefield@gmail.com>
2022-10-19 14:39:08 +0100

    add pytest to requirements, and added additional tests to model unit testing

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,17 +1,25 @@
 import unittest
 import pkmodel as pk
+import pytest
 class ModelTest(unittest.TestCase):
     """
     Tests the :class:`Model` class.
     """
-    def test_create(self):
+
+    @pytest.mark.parametrize(
+    "Centrals, Peripherals, Dosings",
+    [
+        ([5.5, 6.1], [[1,2],[0.001, 2]], []),
+    ])
+    def test_create(self, Centrals, Peripherals, Dosings):
         """
         Tests Model creation.
         """
-        model = pk.model.Model(central=[5.5, 6.1], peripherals=[[1,2],[0.001, 2]], dosing=[])
-        self.assertEqual(model.central, [5.5, 6.1])
-        self.assertEqual(model.peripherals, [[1,2],[0.001, 2]])
-        self.assertEqual(model.dose, [])
+        model = pk.model.Model(central=Centrals, peripherals=Peripherals, dosing=Dosings)
+        self.assertEqual(model.central, Centrals)
+        self.assertEqual(model.peripherals, Peripherals)
+        self.assertEqual(model.dose, Dosings)
+        
 
 
 
