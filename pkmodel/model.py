58ec6ffd00b42c3688c90175578641f214a46b4a
mwhitemfldm <miawhitefield@gmail.com>
2022-10-19 16:15:54 +0100

    add exceptions to model.py

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -2,6 +2,12 @@
 # Model class
 #
 
+# function to catch type error in list
+def catchTypeError(input_list):
+    for val in input_list:
+        if val < 0 or not isinstance(val, (float, int)):
+            raise ValueError('input values should be non-negative numbers')
+
 
 class Model:
     """
@@ -17,9 +23,18 @@ class Model:
     ----------
     pcount: int, number of peripheral compartments
 
+    Exceptions
+    -----------
+    raise value error if a non numerical value or negative number is given as an input
+    raise value error if k_a input is not an empty list, or list containing non negative number
+
     """
-    
     def __init__(self, central, peripherals=[], dosing=[]):
+        catchTypeError(central)
+        for periph in peripherals:
+            catchTypeError(periph)
+        if len(dosing) not in [0,1] or (len(dosing) == 1 and not isinstance(dosing[0], (int, float))):
+            raise ValueError('invalid k_a input')
         self.central = central
         self.peripherals = peripherals
         self.dose = dosing
@@ -27,3 +42,5 @@ class Model:
     @property
     def pcount(self):
         return len(self.peripherals)
+
+
