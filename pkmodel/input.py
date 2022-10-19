006be7516891f00af86447e1b25849b6c36948b7
Jacob Wright <vlad@tran.sylvan.ia>
2022-10-19 14:58:37 +0100

    Modify input.py

--- pkmodel/input.py
+++ pkmodel/input.py
@@ -1,31 +1,63 @@
+def central_input():
+    """
+    Collect central compartment data from user input:
+        Volume, VC / mL
+        Clearance rate, CL / mL/h
 
-def model_input():
+    Returns:
+    central (list): central compartment parameters [VC, CL]
     """
-    Returns parameter arrays from unit inputs for central, peripheral and dosage compartments
-    """
-    # Creating central array
+    # TODO: Fail if input anything except int or float
     VC = float(input('Volume of central compartment in mL: '))
     CL = float(input('Clearance rate from central compartment in mL/h: '))
     central = [VC, CL]
+    
+    return central
 
-    # Creating peripherals array
+
+def peripheral_input():
+    """
+    Collect peripheral compartment(s) data from user input:
+        Number of compartments
+        Volume, V / mL of each compartment
+        Transition rate, Q / mL/h of each compartment
+
+    Returns:
+    peripherals (list of lists): peripheral compartment parameters [[V1, Q1], [V2, Q2]]
+        - empty list if no peripheral compartments
+    """
+    # TODO: Fail if input anything except int
     NUM_OF_PCS = int(input('Number of peripheral compartments (0, 1, or 2): '))
 
     peripherals = []
     for i in range(0,NUM_OF_PCS):
+         # TODO: Fail if input anything except int or float
         VP = float(input('Volume of peripheral compartment {} in mL: '.format(i+1)))
         QP = float(input('Transition rate between central compartment and peripheral compartment {} in mL/h: '.format(i+1)))
         peripherals.append([VP,QP])
+    
+    return peripherals
 
-    # Creating dosing array
+
+
+def dosing_type_input():
+    """
+    Determine dosage compartment existence and parameters from user input:
+        Absorption rate k_a / /h
+
+    Returns:
+    dosing (list): dosage compartment parameters []
+        - empty list if no dosage compartment
+    """
     DOSAGE_COMPARTMENT = input('Is there a dosage compartment (Yes/No)? ')
 
     dosing = []
     if DOSAGE_COMPARTMENT == 'Yes':
-        KA = float(input('Absorption rate for dosage compartment (/h) if subcutaneous dosing: '))
-        dosing.append(KA)
+        k_a = float(input('Absorption rate for dosage compartment (/h) if subcutaneous dosing: '))
+        dosing.append(k_a)
+    
+    return dosing
 
-    return central, peripherals, dosing
 
 def dosage_input():
     """ 
@@ -41,6 +73,7 @@ def dosage_input():
         DOSE = int(input('Instantaneous dose of drug given per time point (ng): '))
         TIME_POINTS = list(input('List of time points at which drug is given (h): '))
 
+    # TODO: Generate function
     # TODO: what if it is a combination of both types of dosage
 
 
