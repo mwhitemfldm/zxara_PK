de7c2bbb389eb2b498902936022499e86fac9a04
G-Shillcock <81102502+G-Shillcock@users.noreply.github.com>
2022-10-21 15:35:52 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ author = 'Anissa Alloula, Marina Luchner, George Shillcock, Mia Whitefield, Jaco
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+  'myst_parser'
   'sphinx.ext.autodoc',
   'sphinx.ext.mathjax',
   'sphinx.ext.viewcode',
