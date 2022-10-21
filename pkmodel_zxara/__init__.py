d644bdce5e6aefbe84d0490d7a6a2a89f6a95278
Jacob <jacob.wright@dtc.ox.ac.uk>
2022-10-21 12:42:28 +0100

    Update init.py

--- pkmodel_zxara/__init__.py
+++ pkmodel_zxara/__init__.py
@@ -4,8 +4,8 @@ It contains functionality for creating, solving, and visualising the solution
 of Parmokinetic (PK) models
 
 """
-# Import version info
-__version__ = importlib.metadata.version(__name__)
+# # Import version info
+# __version__ = importlib.metadata.version(__name__)
 
-# Import main classes
-from .model import Model    # noqa
+# # Import main classes
+# from .model import Model    # noqa
