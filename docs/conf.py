a363564a17a46fda769372200f51d57cf817031e
G-Shillcock <81102502+G-Shillcock@users.noreply.github.com>
2022-10-21 15:28:14 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,12 @@ author = 'Anissa Alloula, Marina Luchner, George Shillcock, Mia Whitefield, Jaco
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.mathjax',
+  'sphinx.ext.viewcode',
+  'sphinx.ext.napoleon'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +28,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = [] # used to be ['_static']
